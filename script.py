98f5bee30cdb01cc38e9b8d20dbaaa6012b5e519
Mushtari Sadia <midnight.scream98@gmail.com>
2022-06-19 19:15:58 +0600

    updated script

--- script.py
+++ script.py
@@ -18,59 +18,57 @@ allowSelfSignedHttps(True) # this line is needed if you use self-signed certific
 # https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
 
 input_df = pd.read_csv('D:\OneDrive\OneDrive - BUET\OneDrive\MVH\DengueDashboard\\train_test_datasets\\test.csv')
-
 # print(input_df.index)
-input_df = input_df
-forecasts = []
-for i,row in input_df.iterrows():
-  # print(row['Temperature'])
-  data =  {
-    "Inputs": {
-      "data": [
-        {
-          "Date": str(row['Date']),
-          "Temperature": row['Temperature'],
-          "Precipitation": row['Precipitation'],
-          "City": row['City']
-        }
-      ]
-    },
-    "GlobalParameters": {
-      "quantiles": [
-        0.025,
-        0.975
-      ]
-    }
+
+all_data = []
+for i, row in input_df.iterrows():
+	all_data += [{"Date": row['Date'] ,"Temperature": row['Temperature'],"Precipitation": row['Precipitation'],"City": row['City']}]
+
+
+data =  {
+  "Inputs": {
+    "data": all_data
+  },
+  "GlobalParameters": {
+    "quantiles": [
+      0.025,
+      0.975
+    ]
   }
+}
 
-  body = str.encode(json.dumps(data))
+forecasts = []
 
-  url = 'http://9fa22fe3-0e2f-44f9-9a3e-c0bfd6eccbfa.eastus2.azurecontainer.io/score'
-  api_key = 'w63iNYH0gR1eok8Kojx7bw6JqW4yYopo' # Replace this with the API key for the web service
+body = str.encode(json.dumps(data))
 
-  # The azureml-model-deployment header will force the request to go to a specific deployment.
-  # Remove this header to have the request observe the endpoint traffic rules
-  headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+url = 'http://9fa22fe3-0e2f-44f9-9a3e-c0bfd6eccbfa.eastus2.azurecontainer.io/score'
+api_key = 'w63iNYH0gR1eok8Kojx7bw6JqW4yYopo' # Replace this with the API key for the web service
 
-  req = urllib.request.Request(url, body, headers)
+# The azureml-model-deployment header will force the request to go to a specific deployment.
+# Remove this header to have the request observe the endpoint traffic rules
+headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
 
-  try:
-      response = urllib.request.urlopen(req)
+req = urllib.request.Request(url, body, headers)
 
-      result = response.read()
-      # print(result.decode())
-      # print(type(json.loads(result.decode('utf-8'))))
-      # df = pd.DataFrame.from_dict(json.loads(result.decode('utf-8')))
-      forecasts.append(json.loads(result.decode('utf-8'))['Results']['forecast'][0])
-      # dfs.append(df)
-      # print(forecasts[-1])
+try:
+	response = urllib.request.urlopen(req)
 
-  except urllib.error.HTTPError as error:
-      print("The request failed with status code: " + str(error.code))
+	result = response.read()
+	# print(result.decode())
+	# print(type(json.loads(result.decode('utf-8'))))
+	# df = pd.DataFrame.from_dict(json.loads(result.decode('utf-8')))
+	forecasts = (json.loads(result.decode('utf-8'))['Results']['forecast'])
+	# dfs.append(df)
+	# print(forecasts[-1])
 
-      # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-      print(error.info())
-      print(error.read().decode("utf8", 'ignore'))
+except urllib.error.HTTPError as error:
+	print("The request failed with status code: " + str(error.code))
+
+	# Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+	print(error.info())
+	print(error.read().decode("utf8", 'ignore'))
+
 
 input_df['Value'] = forecasts
+input_df_2 = input_df
 print(input_df_2)
